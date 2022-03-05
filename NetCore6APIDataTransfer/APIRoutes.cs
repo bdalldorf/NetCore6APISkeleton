585de5ffadf46566c508a0510b33ec3b53cbf0ed
Bret Dalldorf <bret.dalldorf@gmail.com>
2022-03-05 02:12:49 -0800

    feat(app): added API DataTransfer Class Project

---
+++ NetCore6APIDataTransfer/APIRoutes.cs
@@ -0,0 +1,36 @@
+﻿namespace NetCore6APIDataTransfer
+{
+    public static class ApiRoutes
+    {
+        public static class ApiStartupRoute
+        {
+            public const string GetApiStartup = "api/start";
+        }
+
+        public static class ApiAuthenticationRoute
+        {
+            public const string PostWebLoginInformation = "authenticate/web/login";
+            public const string PostWebLoginAntiForgeryCookie = "authenticate/web/antiforgerycookie";
+            public const string PostWebLogoutInformation = "authenticate/web/logout";
+            public const string PostApiLoginInformation = "authenticate/api/login";
+        }
+
+        public static class ApiTestRoute
+        {
+            public const string GetApiTestItems = "apitest/getall/";
+            public const string GetApiTestItem = "apitest/get/{id}";
+            public const string SaveApiTestItem = "apitest/save";
+            public const string UpdateApiTestItem = "apitest/update";
+            public const string DeleteApiTestItem = "apitest/delete";
+        }
+
+        public static class ExampleRoute
+        {
+            public const string GetExampleItems = "example/getall/";
+            public const string GetExampleItem = "example/get/{id}";
+            public const string SaveExampleItem = "example/save";
+            public const string UpdateExampleItem = "example/update";
+            public const string DeleteExampleItem = "example/delete";
+        }
+    }
+}
---
+++ NetCore6APIDataTransfer/Requests/Authentication/TokenRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace NetCore6APIDataTransfer.Requests.Authentication
+{
+    public class TokenRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
